f831067e4588a67b5859e4fa1284e0133ef5e4c0
Dave136 <davejs136@gmail.com>
2022-11-25 12:36:43 -0400

    feat: enable icons for windicss

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 function App() {
   return (
-    <div className="relative">
-      <h1 className="text-center text-3xl underline mt-6">Welcome to my app</h1>
+    <div className="relative ">
+      <div className="flex justify-center items-center mt-8">
+        <h1 className="text-center text-3xl underline mr-4">
+          Welcome to my app
+        </h1>
+        <div className="bg-gray-200 text-gray-800 hover:bg-gray-300 rounded w-12 h-12 flex items-center justify-center">
+          <i className="icon-menu"></i>
+        </div>
+      </div>
     </div>
   );
 }
--- windi.config.ts
+++ windi.config.ts
@@ -1,9 +1,11 @@
 import typography from 'windicss/plugin/typography';
+import icons from '@windicss/plugin-icons';
 import { defineConfig } from 'windicss/helpers';
 
 export default defineConfig({
   darkMode: 'class',
   plugins: [
+    icons,
     typography({
       dark: true,
     }),
